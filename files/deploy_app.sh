146d254c05bd9b6306e2e4614874db24f1592812
Andrea Merlini <merlini_andrea@yahoo.it>
2022-09-07 14:36:09 +0200

    Refactoring welcome message

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. It's a really <b>cool</b> app!.
+  Welcome to the wonderful ${PREFIX}'s app. It's a <b>really cool app!</b>
   
   <!-- END -->
 
